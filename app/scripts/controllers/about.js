05821b7adfbbc399996467beec32338a87eb4385
Toshiyuki Hina <thina@yourinventit.com>
2014-08-10 11:03:16 +0900

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFileUploadClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularFileUploadClientApp
+ */
+angular.module('angularFileUploadClientApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFileUploadClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularFileUploadClientApp
+ */
+angular.module('angularFileUploadClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
